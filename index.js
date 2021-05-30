36efef9beddf2747259a8edc8ebc7b6625f03db7
Nurutomo <tixbig@gmail.com>
2021-05-30 21:54:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-$(document).load(function () {
+$(document).ready(function () {
   const params = Object.fromEntries(new URL(location.href).searchParams.entries())
   console.log(params)
 
-  $('[id|=search-params-]').each(function (i, el) {
+  $('[id^=search-params-]').each(function (i, el) {
     const e = $(el)
     const key = e.attr('id').replace('search-params-', '')
     const value = params[key] != null ? params[key] : ''
